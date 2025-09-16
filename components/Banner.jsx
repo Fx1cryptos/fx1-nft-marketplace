9ad02b0bad6b4e7ca64483573cf6c9ca91551879
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2025-09-16 15:26:18 -0500

    Create Banner.jsx

---
+++ components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => (
+  <div className="banner">
+    <img
+      src="https://ipfs.io/ipfs/bafybeid5mdc6emwyfahsglafto2c2ohomm3svv6wgx4uza7gep7jdiyuxu"
+      alt="FX1 NFT Marketplace Banner"
+      className="banner-img"
+    />
+  </div>
+);
+
+export default Banner;
