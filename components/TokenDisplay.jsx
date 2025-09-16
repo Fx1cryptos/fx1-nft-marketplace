b15944d9582a4c3407de8136e5bde7c78401acc0
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2025-09-16 15:28:21 -0500

    Create TokenDisplay.jsx

---
+++ components/TokenDisplay.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TokenDisplay = () => (
+  <div className="token-display">
+    <h2>$FDH Token</h2>
+    <img
+      src="https://ipfs.io/ipfs/bafybeicxaddlgd7hibu42fifvw7axfwrkkvesfqnlpg6h5fkpsnec4vzcu"
+      alt="$FDH Token"
+      className="token-img"
+    />
+  </div>
+);
+
+export default TokenDisplay;
