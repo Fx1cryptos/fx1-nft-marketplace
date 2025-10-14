e2a8257313f5ae32f3309997120bba102f4fadc8
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2025-10-14 23:08:06 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
-import App from "../components/App";
-import "../styles/globals.css";
-
-export default function Home() {
-  return <App />;
+{
+  "name": "fx1-hubs-miniapp",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "@thirdweb-dev/chains": "^1.2.0",
+    "@thirdweb-dev/react": "^4.0.0",
+    "@thirdweb-dev/sdk": "^4.0.0",
+    "ethers": "^6.0.0",
+    "next": "14.2.0",
+    "react": "18.3.1",
+    "react-dom": "18.3.1",
+    "axios": "^1.6.8"
+  }
 }
